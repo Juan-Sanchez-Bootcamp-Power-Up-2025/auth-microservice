1159423550ff0d17733eb02a4e68229c72864a10
Juan Pablo <juanpasan2000@gmail.com>
2025-08-31 21:45:44 -0500

    Security adapter

---
+++ infrastructure/driven-adapters/security/src/main/java/co/com/crediya/authentication/security/dto/UserSecurity.java
@@ -0,0 +1,51 @@
+package co.com.crediya.authentication.security.dto;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+public class UserSecurity implements UserDetails {
+
+    private String id;
+
+    private String name;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    private String roleId;
+
+    private LocalDate birthDate;
+
+    private String address;
+
+    private String documentId;
+
+    private String phoneNumber;
+
+    private BigDecimal baseSalary;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(roleId));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+}
