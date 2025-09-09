958debde0c143338d9ff09a65b58699f6094e2e7
Juan Pablo <juanpasan2000@gmail.com>
2025-09-08 20:50:24 -0500

    Docker containerization

--- applications/app-service/src/main/resources/db/migration/V1__database_init.sql
+++ applications/app-service/src/main/resources/db/migration/V1__database_init.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 -- ROLES
 CREATE TABLE IF NOT EXISTS roles (
   id VARCHAR(15) PRIMARY KEY,
@@ -20,9 +22,15 @@ CREATE TABLE IF NOT EXISTS users (
   password VARCHAR(120) NOT NULL,
   role_id VARCHAR(15) NOT NULL,
   birth_date DATE,
-  address VARCHAR(120) NOT NULL,
+  address VARCHAR(120),
   document_id VARCHAR(15) NOT NULL UNIQUE,
   phone_number VARCHAR(15),
   base_salary NUMERIC(15) NOT NULL,
   CONSTRAINT fk_user_role FOREIGN KEY (role_id) REFERENCES roles(id)
 );
+
+INSERT INTO users (id, name, last_name, email, password, role_id, document_id, base_salary)
+VALUES
+('332c4918-3e7a-4d64-be0e-8191fb0c3583', 'Admin', 'User', 'admin@crediya.com', '$2a$10$gEmmSCjsm0X38BK9wzD3TeMk/O.Kp/H20RObDldrcQdojdoHvgLpG', 'ADMIN', '123456', 1234),
+('f4dd2fdf-6a72-4d53-882e-bea2fad3f86b', 'Client', 'User', 'client@crediya.com', '$2a$10$ME3rrQKvjQZk5UwoTBCdYOstqOsDHB.4Fbkm3zQQiBhMT0Z0pQ40a', 'CLIENT', '1234567', 54872),
+('9d7d5d28-2423-4684-acbe-ab22ad886041', 'Consultant', 'User', 'consultant@crediya.com', '$2a$10$t99iihzZ7hhwRHv7wp7Ahe6TDEBQZdIBVp8yJgsV0Qj65s4tlK7T.', 'CONSULTANT', '12345678', 1234);
